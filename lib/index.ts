858e247f107983d5a31d43265795838438f5abeb
Matt Young <31349794+mattgoespro@users.noreply.github.com>
2025-09-15 09:52:57 +0200

    export types not previously available bump version

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,3 @@
 export { PrefixNameLogPreset, createLogger } from "./logging/logger";
-export { Logger, LoggerOptions } from "./logging/logger.model";
+export { Logger, LoggerOptions, LogLevel, CreateLogMessageOptions } from "./logging/logger.model";
 export { FormattedError } from "./errors/formatted-error";
